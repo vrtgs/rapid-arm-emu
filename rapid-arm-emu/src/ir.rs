ed3651027b37746bd3bae0f3b29ef8886444863f
Vrtgs <vrtgs@vrtgs.xyz>
2026-05-05 05:56:39 +0300

    WIP

--- rapid-arm-emu/src/ir.rs
+++ rapid-arm-emu/src/ir.rs
@@ -1,19 +1,19 @@
-use crate::ir::arena::make_handle;
+use std::sync::atomic::AtomicU32;
+use crate::armv9::ProcessorState;
+use crate::io_mmu;
+use crate::io_mmu::IoMMU;
+use crate::ir::arena::{impl_storable, Arena};
 
 mod arena;
 
 
-// unsafe extern "C" fn(
-//     processor_state: &mut ProcessorState,
-//     pages: *const io_mmu::Page,
-//     page_count: u64,
-//     halt_reason: *const AtomicU32,
-//     io_mmu: *const IoMMU,
-// )
-
-
-make_handle!(Value);
-make_handle!(Stmt);
+type ExecBlock = unsafe extern "C" fn(
+    processor_state: &mut ProcessorState,
+    pages: *const io_mmu::Page,
+    page_count: u64,
+    halt_reason_ptr: *const AtomicU32,
+    io_mmu: *const IoMMU,
+) -> u32;
 
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -26,7 +26,7 @@ pub enum IntWidth {
 
 impl IntWidth {
     pub const fn bits(self) -> u32 {
-        self as u32 * 8
+        (self as u32).strict_mul(8)
     }
 
     pub const fn bytes(self) -> usize {
@@ -36,116 +36,47 @@ impl IntWidth {
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Type {
+    Unit,
     Int(IntWidth),
-    Ptr,
+    HostPtr,
 }
 
-
-#[derive(Debug, Clone)]
-pub struct StmtData {
-    pub kind: StmtKind,
-    pub result: Option<Value>,
+pub struct LValueData {
+    pub ty: Type,
 }
 
-#[derive(Debug, Clone)]
-pub enum StmtKind {
-    /// Integer constant.
-    Iconst {
-        width: IntWidth,
-        value: u64,
-    },
+// Conceptual signature of every translated basic block:
 
-    /// Integer arithmetic.
-    ///
-    /// `lhs` and `rhs` must have type `Int(width)`.
-    /// The result also has type `Int(width)`.
-    Arith {
-        op: ArithOp,
-        width: IntWidth,
-        lhs: Value,
-        rhs: Value,
-    },
-
-    /// Load from a host pointer plus a constant byte offset.
-    ///
-    /// This is used for things like reading `ProcessorState` fields:
-    ///
-    /// ```text
-    /// LoadHost64(processor_state, offset_of!(ProcessorState, x_registers) + 8 * n)
-    /// ```
-    LoadHost {
-        width: IntWidth,
-        base: Value,
-        offset: usize,
-    },
-
-    /// Store to a host pointer plus a constant byte offset.
-    StoreHost {
-        width: IntWidth,
-        base: Value,
-        offset: usize,
-        value: Value,
-    },
-
-    /// Load from VM memory through the `IoMMU`.
-    ///
-    /// `vaddr` must be an `i64` value.
-    LoadVm {
-        width: IntWidth,
-        io_mmu: Value,
-        vaddr: Value,
-    },
-
-    /// Store to VM memory through the `IoMMU`.
-    ///
-    /// `vaddr` must be an `i64` value.
-    /// `value` must have type `Int(width)`.
-    StoreVm {
-        width: IntWidth,
-        io_mmu: Value,
-        vaddr: Value,
-        value: Value,
-    },
+impl_storable! {
+    LValueData as impl pub LValue;
+    init: {
+        const ARG_PROCESSOR_STATE = LValueData { ty: Type::HostPtr };
+        const ARG_PAGES = LValueData { ty: Type::HostPtr };
+        const ARG_PAGE_COUNT = LValueData { ty: Type::Int(IntWidth::W64) };
+        const ARG_HALT_REASON_PTR = LValueData { ty: Type::HostPtr };
+        const ARG_IO_MMU = LValueData { ty: Type::HostPtr };
+    }
 }
 
-pub struct SSABlock {
-
-    terminator: Option<Terminator>,
-}
-
-
-
-
-/// Conceptual signature of every translated basic block:
-///
-/// ```text
-/// fn(
-///     io_mmu: *const IoMMU,
-///     halt_reason: *const AtomicU32,
-///     processor_state: *mut ProcessorState,
-/// ) -> NonZero<u32>
-/// ```
-#[derive(Debug, Clone)]
-pub struct VMBasicBlockBuilder {
-    values: Vec<ValueData>,
-    insts: Vec<InstData>,
-    params: BasicBlockParams,
-}
-
-
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct BasicBlockParams {
-    pub processor_state: Value,
-    pub io_mmu: Value,
-    pub halt_reason: Value,
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum PointerKind {
-    IoMmu,
-    AtomicU32,
+pub enum Arg {
     ProcessorState,
+    Pages,
+    PageCount,
+    HaltReasonPtr,
+    IoMMU
+}
+
+impl LValue {
+    pub fn as_arg(self) -> Option<Arg> {
+        match self {
+            Self::ARG_PROCESSOR_STATE => Some(Arg::ProcessorState),
+            Self::ARG_PAGES => Some(Arg::Pages),
+            Self::ARG_PAGE_COUNT => Some(Arg::PageCount),
+            Self::ARG_HALT_REASON_PTR => Some(Arg::HaltReasonPtr),
+            Self::ARG_IO_MMU => Some(Arg::IoMMU),
+            _ => None
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -168,132 +99,110 @@ pub enum ArithOp {
     Div,
 }
 
-#[derive(Debug, Clone)]
-pub struct ValueData {
-    pub ty: Type,
-    pub def: ValueDef,
-}
 
 #[derive(Debug, Clone)]
-pub enum ValueDef {
-    Param(Param),
-    Inst(Inst),
+pub enum RValue {
+    /// Integer constant.
+    Iconst {
+        width: IntWidth,
+        value: u64,
+    },
+
+    /// Integer arithmetic.
+    ///
+    /// `lhs` and `rhs` must have type `Int(width)`.
+    /// The result also has type `Int(width)`.
+    Arith {
+        op: ArithOp,
+        width: IntWidth,
+        lhs: LValue,
+        rhs: LValue,
+    },
+
+    /// Load from a host pointer plus a constant byte offset.
+    ///
+    /// This is used for things like reading `ProcessorState` fields:
+    ///
+    /// ```text
+    /// LoadHost64(processor_state, offset_of!(ProcessorState, x_registers) + 8 * n)
+    /// ```
+    LoadHost {
+        width: IntWidth,
+        base_ptr: LValue,
+        offset: usize,
+    },
+
+    /// Store to a host pointer plus a constant byte offset.
+    StoreHost {
+        width: IntWidth,
+        base_ptr: LValue,
+        offset: usize,
+        value: LValue,
+    },
+
+    LoadHaltReason(LValue),
 }
 
-#[derive(Debug, Clone)]
-pub struct Param {
-    pub index: u8,
-    pub name: &'static str,
+
+pub struct Stmt {
+    pub lvalue: LValue,
+    pub rvalue: RValue,
 }
 
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Terminator {
-    /// Return a non-zero `u32` block-exit reason.
-    ///
-    /// The IR expects this value to be non-zero. The builder can check the type,
-    /// but cannot prove non-zero-ness for arbitrary SSA values.
-    ReturnNonZero {
-        value: Value,
+    /// Return "0" i.e. return success.
+    Return,
+    /// Return a `NonZero<u32>` block-exit reason.
+    ReturnFail {
+        value: LValue
     },
+    BrNZ {
+        cond: LValue,
+        non_zero: Block,
+        zero: Block,
+    },
+    Br(Block),
 }
 
-impl BasicBlockIr {
+pub struct BlockData {
+    pub stmts: Vec<Stmt>,
+    pub terminator: Terminator,
+    pub is_cold: bool,
+}
+
+
+impl_storable!(
+    BlockData as impl pub Block;
+    init: {
+        const ENTRYPOINT = BlockData {
+            stmts: vec![],
+            terminator: Terminator::Return,
+            is_cold: false,
+        };
+    }
+);
+
+
+pub struct ExecIrBuilder {
+    pub lvalues: Arena<LValueData>,
+    pub blocks: Arena<BlockData>,
+    pub current_block: Block,
+}
+
+impl ExecIrBuilder {
     pub fn new() -> Self {
-        let mut ir = Self {
-            values: Vec::new(),
-            insts: Vec::new(),
-            params: BasicBlockParams {
-                io_mmu: Value(u32::MAX),
-                halt_reason: Value(u32::MAX),
-                processor_state: Value(u32::MAX),
-            },
-            terminator: None,
-        };
-
-        let io_mmu = ir.push_param(Type::Ptr(PointerKind::IoMmu), 0, "io_mmu");
-        let halt_reason = ir.push_param(Type::Ptr(PointerKind::AtomicU32), 1, "halt_reason");
-        let processor_state =
-            ir.push_param(Type::Ptr(PointerKind::ProcessorState), 2, "processor_state");
-
-        ir.params = BasicBlockParams {
-            io_mmu,
-            halt_reason,
-            processor_state,
-        };
-
-        ir
+        Self {
+            lvalues: Arena::new(),
+            blocks: Arena::new(),
+            current_block: Block::ENTRYPOINT
+        }
     }
 
-    pub fn params(&self) -> BasicBlockParams {
-        self.params
+    #[must_use]
+    fn make_lvalue(&mut self, ty: Type) -> LValue {
+        self.lvalues.store(LValueData { ty })
     }
 
-    pub fn values(&self) -> &[ValueData] {
-        &self.values
-    }
-
-    pub fn insts(&self) -> &[InstData] {
-        &self.insts
-    }
-
-    pub fn terminator(&self) -> Option<Terminator> {
-        self.terminator
-    }
-
-    pub fn value_type(&self, value: Value) -> Type {
-        self.values[value.index()].ty
-    }
-
-    fn push_param(
-        &mut self,
-        ty: Type,
-        index: u8,
-        name: &'static str,
-    ) -> Value {
-        self.push_value(ValueData {
-            ty,
-            def: ValueDef::Param(Param { index, name }),
-        })
-    }
-
-    fn push_value(&mut self, data: ValueData) -> Value {
-        let index = self.values.len();
-        let value = Value(u32::try_from(index).unwrap());
-        self.values.push(data);
-        value
-    }
-
-    fn push_inst(&mut self, kind: StmtKind, result_ty: Option<Type>) -> (Inst, Option<Value>) {
-        let inst_index = self.insts.len();
-        let inst = Inst(u32::try_from(inst_index).unwrap());
-
-        let result = result_ty.map(|ty| {
-            self.push_value(ValueData {
-                ty,
-                def: ValueDef::Inst(inst),
-            })
-        });
-
-        self.insts.push(InstData { kind, result });
-
-        (inst, result)
-    }
 }
-
-impl Value {
-    pub const fn raw(self) -> u32 {
-        self.0
-    }
-
-    fn index(self) -> usize {
-        self.0 as usize
-    }
-}
-
-impl Inst {
-    pub const fn raw(self) -> u32 {
-        self.0
-    }
-}
-
--- rapid-arm-emu/src/ir/arena.rs
+++ rapid-arm-emu/src/ir/arena.rs
@@ -48,8 +48,18 @@ impl RawHandle {
     }
 
     #[track_caller]
-    pub fn new(index: usize) -> Self {
-        Self::try_new(index).unwrap_or_else(|| panic!("SSA handle overflow"))
+    pub const fn new(index: usize) -> Self {
+        match Self::try_new(index) {
+            Some(handle) => handle,
+            None => panic!("SSA handle overflow")
+        }
+    }
+
+    pub const fn inc(self) -> Option<Self> {
+        match self.0.checked_add(1) {
+            Some(nz) => Some(Self(nz)),
+            None => None
+        }
     }
 
     pub const fn get(self) -> usize {
@@ -172,12 +182,113 @@ macro_rules! make_handle {
         #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
         #[repr(transparent)]
         $vis struct $name($crate::ir::arena::RawHandle);
-        
+
         const _: () = {
             unsafe impl $crate::ir::arena::Handle for $name {}
         };
     };
 }
 
+macro_rules! handle_impl_helper {
+    (
+        impl const for $name: path {
+            $(const $const_name: ident;)*
+        }
+    ) => {
+        impl $name {
+            $crate::ir::arena::handle_impl_helper! {
+                @fold
+                current_stack: [],
+                munching: { $(const $const_name;)* }
+            }
+        }
+    };
+    
+    (
+        @fold
+        current_stack: [$($tt:tt),*],
+        munching: {
+            const $const_name: ident;
+            $($rest:tt)*
+        }
+    ) => {
+        const $const_name: Self = {
+            match $crate::ir::arena::RawHandle::try_new(<[()]>::len(&[$($tt),*])) {
+                Some(x) => $crate::ir::arena::from_raw(x),
+                None => panic!("too many constants declared")
+            }
+        };
 
-pub(super) use make_handle;
+        $crate::ir::arena::handle_impl_helper! {
+            @fold
+            current_stack: [$($tt,)* ()],
+            munching: { $($rest)* }
+        }
+    };
+
+    (
+        @fold
+        current_stack: [$($tt:tt),*],
+        munching: {}
+    ) => {};
+}
+
+macro_rules! impl_storable {
+    (
+        $ty: ty as $(impl $vis: vis $impl_name: ident)? $(($existing_handle: path))?$(;$(init: {
+            $(const $const_name: ident = $init: expr;)*
+        })?)?
+    ) => {
+
+        $($crate::ir::arena::make_handle!($vis $impl_name);)?
+
+
+        $($crate::ir::arena::handle_impl_helper! {
+            impl const for $impl_name {
+                $($(const $const_name;)*)?
+            }
+        })?
+
+        const _: () = { 
+            #[allow(dead_code)]
+            const LEN: usize = {
+                #[allow(unused_variables, unused_mut)]
+                let mut is_impl = false;
+                $(
+                let _ = ::core::stringify!($impl_name);
+                is_impl |= true;
+                )?
+
+                0 $($(+ {
+                    assert!(
+                        is_impl,
+                        "if storable is using an exisiting handle, it can't add const"
+                    );
+
+                    #[allow(non_snake_case, unused_variables)]
+                    {
+                        <[()]>::len(&[$({ let $const_name: (); }),*])
+                    }
+                })?)?
+            };
+
+            impl $crate::ir::arena::Storable for $ty {
+                type Handle = $($existing_handle)? $($impl_name)?;
+
+                const INITIAL_VEC_LEN: usize = LEN;
+
+                fn initial_vec() -> Vec<Self> {
+                    ::std::vec![$($($($init),*)?)?]
+                }
+            }
+        };
+
+    };
+}
+
+
+#[doc(hidden)]
+#[allow(unused_imports)]
+pub(super) use {handle_impl_helper};
+
+pub(super) use {make_handle, impl_storable};
